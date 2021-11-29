984aa55c6bcdbb76b4336fc326b64f86414527b1
Leiyi548 <1424630446@qq.com>
2021-11-29 15:32:22 +0800

    delete notuseful configure

--- lua/nv-telescope/init.lua
+++
@@ -1,5 +0,0 @@
---
--- keymapping setting
---
-vim.api.nvim_set_keymap('n','<leader>ff',':Telescope find_files<CR>',{ noremap=true,silent=true })
-vim.api.nvim_set_keymap('n','<leader>fb',':Telescope buffers<CR>',{ noremap=true,silent=true })
--- lua/nv-tmuxNavigator/init.lua
+++
@@ -1,5 +0,0 @@
-vim.g.tmux_navigator_no_mappings=1
-vim.api.nvim_set_keymap('n','<C-l>',':TmuxNavigateLeft<CR>',{ noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<C-j>',':TmuxNavigateDown<CR>',{ noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<C-k>',':TmuxNavigateUp<CR>',{ noremap=true,silent=true})
-vim.api.nvim_set_keymap('n','<C-r>',':TmuxNavigateRight<CR>',{ noremap=true,silent=true})
